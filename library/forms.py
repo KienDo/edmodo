33a8ba3749317bab0273c0761f76aa25df4eaec1
Do Duc Kien <skzquan@yahoo.com.vn>
2012-03-16 20:58:36 +0700

    our first demo

---
+++ library/forms.py
@@ -0,0 +1,119 @@
+import re
+from django import forms
+from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
+from library.models import *
+from django.forms import ModelForm
+from django.utils.safestring import mark_safe
+
+class LoginForm(forms.Form):
+    username=forms.CharField(label='Username',max_length=200)
+    password=forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput(),
+    )
+class RegistrationForm(forms.Form):
+    username = forms.CharField(label='Username', max_length=30)
+    email = forms.EmailField(label='Email')
+    password1 = forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput()
+    )
+    password2 = forms.CharField(
+        label='Password (Again)',
+        widget=forms.PasswordInput()
+    )
+    def clean_password2(self):
+        if 'password1' in self.cleaned_data:
+            password1 = self.cleaned_data['password1']
+            password2 = self.cleaned_data['password2']
+            if password1 == password2:
+                return password2
+        raise forms.ValidationError('Passwords do not match.')
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if not re.search(r'^\w+$', username):
+            raise forms.ValidationError('Username can only contain alphanumeric characters and the underscore.')
+        try:
+            User.objects.get(username=username)
+        except ObjectDoesNotExist:
+            return username
+        raise forms.ValidationError('Username is already taken.')
+
+class UploadFileForm(forms.Form):
+    title=forms.CharField(
+        label='Title',max_length=200,
+        #widget=forms.TextInput(attrs={'size':80})
+    )
+
+    up_file=forms.FileField(
+        label='File URL',
+        #widget=forms.FileInput(attrs={'size':80,'margin-right':})
+    )
+
+    description=forms.Field(
+        widget=forms.Textarea(),
+        label='Description',
+    )
+    '''
+    category=forms.ChoiceField(
+        choices='Kien,dep,trai',
+        label='Category',
+    )
+    '''
+    category=forms.ModelChoiceField(
+        queryset=Category.objects.all(),
+        label='Category',
+        initial='',
+    )
+    public_share=forms.BooleanField(
+        initial=False,
+        label='Public Sharing',
+    )
+
+    title.widget.attrs['size']=50
+    up_file.widget.attrs['size']=50
+    description.widget.attrs['cols']=50
+    #Co the dung text boi doan ma duoc chen vao trong the html
+    category.widget.attrs['style']='width:150'
+
+
+class HorizRadioRenderer(forms.RadioSelect.renderer):
+    """ this overrides widget method to put radio buttons horizontally
+        instead of vertically.
+    """
+    def render(self):
+        """Outputs radios"""
+        return mark_safe(u'\n'.join([u'%s\n' % w for w in self]))
+GENDER_CHOICES=[('m','Male'),('f','Female')]
+class UserInfoForm(forms.Form):
+    first_name=forms.CharField(label='First name',max_length=200)
+    last_name=forms.CharField(label='Last name',max_length=200)
+    gender=forms.ChoiceField(
+        label='Gender',
+        widget=forms.RadioSelect(renderer=HorizRadioRenderer),
+        choices=GENDER_CHOICES,
+        initial='m',
+    )
+    birth_date=forms.DateField()
+    email=forms.EmailField(label='Email')
+    profile_image=forms.ImageField(
+        label="Profile Picture"
+    )
+
+
+FILTER_CHOICES=[
+    ('posted_date','Posted Date'),
+    ('reading_number','Reading Number'),
+    ('like_number','Like Number'),
+    ('file_size','File Size'),
+]
+class FilterForm(forms.Form):
+    main_filter=forms.ChoiceField(
+        label='',
+        choices=FILTER_CHOICES,
+    )
+    sub_filter=forms.ChoiceField(
+        label='',
+        choices=[('dec','Decrease'),('inc','Increase')],
+    )
--- urls.py
+++ urls.py
@@ -1,17 +1,46 @@
-from django.conf.urls.defaults import patterns, include, url
-
+from django.conf.urls.defaults import *
+from django.views.generic.simple import direct_to_template
+from library.views import *
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
+site_media=os.path.join(
+    os.path.dirname(__file__),'site_media'
+)
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'edmodo.views.home', name='home'),
-    # url(r'^edmodo/', include('edmodo.foo.urls')),
+    # Site media
+    url(r'^site_media/(?P<path>.*)$','django.views.static.serve',
+            {'document_root': site_media }),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
+    #Admin doc
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^$',main_page),
+
+    #Login , logout ,register
+    url(r'^accounts/login/$',login),
+    url(r'^accounts/logout/$',logout),
+    url(r'^register/$',register),
+    #User page
+    url(r'^user/(\w+)/$',user_page),
+    url(r'^user/(\w+)/config/$',user_config),
+
+    #Library
+    url(r'^library/$',library),
+    url(r'^library/(\w+)/$',category),
+    url(r'^library/(\w+)/(\d+)',book_page),
+
+
+    #Test cloud
+    url(r'^tag_cloud/$',tag_cloud),
+
+    url(r'^upload/$',file_upload),
+    url(r'^upload/success/$',file_upload_success),
+    url(r'^download/(\d+)/$',file_download),
+
+    url(r'^pdf_display/$',direct_to_template,{'template':'pdf_display.html'}),
+
+
 )
